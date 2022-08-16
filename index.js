45bc1b7f5aa8e24e70af02acbe713e02daa8f0de
Federico Diaz <federico.diaz4@gmail.com>
2022-08-16 17:47:47 -0300

    Agregado manejo archivos y try/catch

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import Contenedor from './contenedor.js';
 
 const PORT = 8080;
 const app = express();
 const router = express.Router();
-const productos = [];
+const archivo = new Contenedor('./productos.txt');
 
 app.use(express.static('public'));
 app.use('/api/productos', router);
@@ -11,41 +12,57 @@ app.use('/api/productos', router);
 router.use(express.json());
 router.use(express.urlencoded({extended: true}));
 
-router.get('/', (req,res) => {
-    res.json(productos)
-});
-
-router.get('/:id', (req,res) => {
-    const data = productos.find(p=>p.id == req.params.id);
-    return data !== undefined ? res.status(200).json(data) : res.status(400).json({error: "Producto no encontrado"})
-});
-
-router.post('/', (req, res) => {
-    const {title, price, thumbnail} = req.body;
-    const prod = {id: productos.length + 1, title:title, price:price, thumbnail: thumbnail}
-    productos.push(prod);
-    return res.status(200).json(prod);
-});
-
-router.put('/:id', (req,res) => {
-    const data = productos.find(p=>p.id == req.params.id);
-    if (data === undefined) {
-        return res.status(400).json({error: "Producto no encontrado"})
+router.get('/', async (req,res) => {
+    try {
+        const productos = await archivo.getAll();
+        return res.status(200).json(productos);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({error:"No se encuentran productos"});
     }
+});
+
+router.get('/:id', async (req,res) => {
+    try {
+        const data = await archivo.getById(req.params.id);
+        return data !== null ? res.status(200).json(data) : res.status(400).json({error: "Producto no encontrado"})    
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({error:"No se encuentran productos"});
+    }
+});
+
+router.post('/', async (req, res) => {
     const {title, price, thumbnail} = req.body;
-    data.title = title;
-    data.price = price;
-    data.thumbnail = thumbnail;
-    productos[productos.indexOf(p=>p.id == req.params.id)] = data;
-    res.status(200).json({data});
+    try {
+        const prod = await archivo.save({title:title, price:price, thumbnail: thumbnail})
+        return res.status(200).json(prod);
+    } catch (error) {
+        return res.status(500).json({error:"No se pudo agregar el productos"});
+    }
+});
+
+router.put('/:id', async (req,res) => {
+    try {
+        const data = req.body
+        if (data === null) {
+            return res.status(500).json({error: "Producto no encontrado"});
+        }
+        data.id = req.params.id;
+        await archivo.edit(req.params.id, data);
+        res.status(200).json(data);
+    } catch (error) {
+        return res.status(500).json({error: "No se pudo editar el articulo"});
+    }
 })
 
-router.delete('/:id', (req,res) => {
-    const dataDeleted = productos.splice(productos.indexOf(p=>p.id == req.params.id), 1) || undefined;
-    if (dataDeleted === undefined) {
-        return res.status(400).json({error: "Producto no encontrado"})
+router.delete('/:id', async (req,res) => {
+    try {
+        await archivo.deleteById(req.params.id);    
+        res.status(200).json({success: "Producto eliminado"});
+    } catch (error) {
+        return res.status(500).json({error: "Producto no encontrado"})
     }
-    res.status(200).json(productos);
 })
 
 const server = app.listen(PORT, () => {
